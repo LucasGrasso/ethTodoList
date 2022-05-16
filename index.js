6044bca42fe0a9040ae8cd4703c3a0c0b6553e46
NicoHalpe <69744829+NicoHalpe@users.noreply.github.com>
2022-05-16 14:53:50 -0300

    Mobile working

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+if(typeof ethereum === "undefined") {
+    const link = "https://metamask.app.link/dapp/" + location.host;
+    const a = document.createElement("a");
+    a.href = link;
+    a.innerHTML = "Instalar Metamask";
+    document.body.innerHTML = "";
+    document.body.appendChild(a);
+}
 web3 = new Web3(ethereum);
 
 var todoListAddress = "0x9f976e4cbC777B124609d72786B6dAAE01F42a22";
 let todoList = new web3.eth.Contract(todoListABI, todoListAddress);
 let list = [];
+
+const provider = new ethers.providers.Web3Provider(ethereum);
+const signer = provider.getSigner();
+const contract = new ethers.Contract(todoListAddress, todoListABI, signer);
+
 ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
-	let a = ""
+	let a = "";
 	for (let i = 0; i < 6; i++) {
-		a += `#${accounts[0].slice(2 + (6 * i), 8 + (6 * i))},`;
-		list.push(`#${accounts[0].slice(2 + (6 * i), 8 + (6 * i))}`)
+		a += `#${accounts[0].slice(2 + 6 * i, 8 + 6 * i)},`;
+		list.push(`#${accounts[0].slice(2 + 6 * i, 8 + 6 * i)}`);
 	}
 	document.body.style.setProperty("--color", `linear-gradient(90deg, ${a.slice(0, -1)})`);
 	document.body.style.setProperty("--color-simple", list[0]);
@@ -27,11 +40,13 @@ ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
 		})
 		.on("changed", (changed) => console.log("changed", changed))
 		.on("error", (err) => console.log("err", err))
-		.on("connected", (str) => console.log("connected", str));
+		.on("connected", (str) => {
+			console.log("connected", str);
+		});
 });
 
 async function crearTarea() {
-	todoList.methods.crearTarea($('#nuevaTarea').val()).send({ from: ethereum.selectedAddress });
+	todoList.methods.crearTarea($("#nuevaTarea").val()).send({ from: ethereum.selectedAddress });
 	startLoading();
 	document.querySelector(".form-container").reset();
 }
@@ -47,7 +62,7 @@ async function completarTarea(_addr, _id) {
 }
 
 async function getContTareas(_addr) {
-	const result = await todoList.methods.contTareas(_addr).call()
+	const result = await todoList.methods.contTareas(_addr).call();
 	return result;
 }
 
@@ -55,16 +70,16 @@ const startLoading = () => {
 	document.getElementById("loading").style.display = "";
 	rings = document.getElementsByClassName("loading")[0];
 	rings.style.setProperty("--color-simple", list[0]);
-}
+};
 const stopLoading = () => {
 	document.getElementById("loading").style.display = "none";
-}
+};
 
 const template = `<div class="taskTemplate" class="checkbox" style="display: none">
 <input type="checkbox" />
 <span class="content">Contenido va aca</span>
 <button class="btn btn-danger" onclick="eliminarTarea(ethereum.selectedAddress, this.name)">Eliminar</button>
-</div>`
+</div>`;
 
 let templateEl = document.createElement("div");
 templateEl.innerHTML = template;
@@ -74,45 +89,45 @@ async function renderTareas() {
 	document.querySelector(".form-container").reset();
 	document.querySelector("#listaTareas").innerHTML = "";
 	document.querySelector("#tareasCompletadas").innerHTML = "";
-	let contTareas = await getContTareas(ethereum.selectedAddress)
+	let contTareas = await getContTareas(ethereum.selectedAddress);
 	//const color = list[Math.floor(Math.random() * 6)]
 	const color = list[0];
 	for (var i = 1; i <= contTareas; i++) {
-		const tarea = await todoList.methods.tareas(ethereum.selectedAddress, i).call()
-		const Id = tarea.id
-		const contenido = tarea.content
-		const status = tarea.completado
+		const tarea = await todoList.methods.tareas(ethereum.selectedAddress, i).call();
+		const Id = tarea.id;
+		const contenido = tarea.content;
+		const status = tarea.completado;
 		if (contenido === "") continue;
 		$newTaskTemplate = $(".taskTemplate").first().clone();
 		//console.log($newTaskTemplate.find("input")[0])
-		const brightness = tinycolor(color).getBrightness()
+		const brightness = tinycolor(color).getBrightness();
 		$newTaskTemplate.find("input")[0].style.setProperty("--color-simple", color);
-		$newTaskTemplate.find("input")[0].style.setProperty("--text-color", brightness > 128 ? "black" : "white");
-		$newTaskTemplate.find('.content').html(contenido)
-		$newTaskTemplate.find('input')
-			.prop('name', Id)
-			.prop('checked', status)
-			.on('click', () => completarTarea(ethereum.selectedAddress, Id))
-		$newTaskTemplate.find('button')
-			.on('click', () => eliminarTarea(ethereum.selectedAddress, Id))
+		$newTaskTemplate
+			.find("input")[0]
+			.style.setProperty("--text-color", brightness > 128 ? "black" : "white");
+		$newTaskTemplate.find(".content").html(contenido);
+		$newTaskTemplate
+			.find("input")
+			.prop("name", Id)
+			.prop("checked", status)
+			.on("click", () => completarTarea(ethereum.selectedAddress, Id));
+		$newTaskTemplate.find("button").on("click", () => eliminarTarea(ethereum.selectedAddress, Id));
 
 		if (status) {
-			$('#tareasCompletadas').append($newTaskTemplate)
+			$("#tareasCompletadas").append($newTaskTemplate);
 		} else {
-			$('#listaTareas').append($newTaskTemplate)
+			$("#listaTareas").append($newTaskTemplate);
 		}
 
-		$newTaskTemplate.show()
-
+		$newTaskTemplate.show();
 	}
 }
 
-address = null
+address = null;
 
 setInterval(() => {
 	if (ethereum.selectedAddress !== address) {
-		address = ethereum.selectedAddress
+		address = ethereum.selectedAddress;
 		renderTareas();
 	}
 }, 1000);
-
